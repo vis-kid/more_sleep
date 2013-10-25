5f0348020da515f56fe92de309f21a85223b84db
ed wassermann <vis0vis@gmail.com>
2013-10-25 14:17:41 +0200

    parallax scroll piggie text

--- app/assets/javascripts/parallax.js
+++ app/assets/javascripts/parallax.js
@@ -9,4 +9,5 @@ function parallax() {
   var scrollPosition = $(window).scrollTop();
 	$('#Desktop_Pic').css('top', (0 - (scrollPosition * .4 )) + 'px');
 	$('.background_pattern').css('top', (0 - (scrollPosition * .25)) + 'px');
+	$('#piggie_text').css('top', (0 - (scrollPosition * .5)) + 'px');
 }
