a5af5c7f55e2251ff9c6190c4403b04beb130f2e
ed wassermann <vis0vis@gmail.com>
2013-10-27 17:37:40 +0100

    add workmabuddoff picture responsive

--- app/assets/javascripts/parallax.js
+++ app/assets/javascripts/parallax.js
@@ -11,12 +11,16 @@ function parallax() {
 	Background_Pattern = $('.background_pattern');
 	Piggie_Text = $('#piggie_text');
 	Sinatra = $('#sinatra');
-	Applying = $('#applying');
+	Applying = $('#applying_text');
+	Dreamer = $('#dreamer');
+	Dreamer_Text = $('#dreamer_text');
 
 	Desktop_Pic.css('top', (0 - (scrollPosition * .4 )) + 'px');
 	Background_Pattern.css('top', (0 - (scrollPosition * .25)) + 'px');
 	Piggie_Text.css('top', (0 - (scrollPosition * .5)) + 'px');
 	Sinatra.css('top', (0 - (scrollPosition * .5)) + 'px');
 	Applying.css('top', (0 - (scrollPosition * 1)) + 'px');
+	Dreamer.css('top', (0 - (scrollPosition * .5)) + 'px');
+	Dreamer_Text.css('top', (0 - (scrollPosition * 1)) + 'px');
 }
 
